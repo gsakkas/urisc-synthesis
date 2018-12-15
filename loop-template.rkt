22b4e6a486e37459422b7b0caf3ed863e7a5843e
George Sakkas <george.p.sakkas@gmail.com>
2018-12-15 02:22:57 -0800

    Updated loop template

--- loop-template.rkt
+++ loop-template.rkt
@@ -11,12 +11,12 @@
 (define (loop-template a b regs)
   (vector-set! regs 1 a)
   (vector-set! regs 2 b)
-  (define line10 (?? boolean?))
-  (define line11 (?? boolean?))
-  (define line12 (?? boolean?))
-  (define line13 (?? boolean?))
-  (define line3 (?? boolean?))
-  (define line4 (?? boolean?))
+  (define line10 #t)
+  (define line11 #f)
+  (define line12 #f)
+  (define line13 #f)
+  (define line3 #f)
+  (define line4 #f)
   (define r1_0 (choose 0 1 2 3 4 5 6 7 8 9 10 11))
   (define r2_0 (choose 0 1 2 3 4 5 6 7 8 9 10 11))
   (define r1_10 (choose 0 1 2 3 4 5 6 7 8 9 10 11))
@@ -36,18 +36,28 @@
   (define r1_5 (choose 0 1 2 3 4 5 6 7 8 9 10 11))
   (define r2_5 (choose 0 1 2 3 4 5 6 7 8 9 10 11))
   ;; L0
+  (set-subleq regs r1_0 r2_0)
+  ;; L1_0
+  (cond [line10 (set-subleq regs r1_10 r2_10)])
+  ;; L1_1
+  (cond [line11 (set-subleq regs r1_11 r2_11)])
+  ;; L1_2
+  (cond [line12 (set-subleq regs r1_12 r2_12)])
+  ;; L1_3
+  (cond [line13 (set-subleq regs r1_13 r2_13)])
+  ;; L2
+  (set-subleq regs r1_2 r2_2)
+  ;; L3
+  (cond [line3 (set-subleq regs r1_3 r2_3)])
+  ;; L4
+  (cond [line4 (set-subleq regs r1_4 r2_4)])
+  ;; L5
+  (set-subleq regs r1_5 r2_5)
   (cond
-    [(and (bvsle b (bv 0 8)) (bvsge b (bv 5 8))) (bv 0 8)]
+    [(bvsgt (vector-ref regs r2_5) (bv 0 8))
+     ;; Exit
+     (vector-ref regs 0)]
     [else
-     (set-subleq regs r1_0 r2_0)
-     ;; L1_0
-     (cond [line10 (set-subleq regs r1_10 r2_10)])
-     ;; L1_1
-     (cond [line11 (set-subleq regs r1_11 r2_11)])
-     ;; L1_2
-     (cond [line12 (set-subleq regs r1_12 r2_12)])
-     ;; L1_3
-     (cond [line13 (set-subleq regs r1_13 r2_13)])
      ;; L2
      (set-subleq regs r1_2 r2_2)
      ;; L3
@@ -108,22 +118,8 @@
                  (cond [line4 (set-subleq regs r1_4 r2_4)])
                  ;; L5
                  (set-subleq regs r1_5 r2_5)
-                 (cond
-                   [(bvsgt (vector-ref regs r2_5) (bv 0 8))
-                    ;; Exit
-                    (vector-ref regs 0)]
-                   [else
-                    ;; L2
-                    (set-subleq regs r1_2 r2_2)
-                    ;; L3
-                    (cond [line3 (set-subleq regs r1_3 r2_3)])
-                    ;; L4
-                    (cond [line4 (set-subleq regs r1_4 r2_4)])
-                    ;; L5
-                    (set-subleq regs r1_5 r2_5)
-                    ;; Exit
-                    (vector-ref regs 0)
-                    ])
+                 ;; Exit
+                 (vector-ref regs 0)
                  ])
               ])
            ])
@@ -134,11 +130,13 @@
 ;; (println (loop-template (bv 3 8) (bv 1 8) (vector (bv 0 8) (bv 0 8) (bv 0 8) (bv 0 8) (bv 0 8) (bv 0 8) (bv 0 8) (bv 0 8) (bv -1 8) (bv 1 8))))
 
 (define (simple-mult a b regs)
-  (if (and (bvsle b (bv 0 8)) (bvsge b (bv 5 8))) (bv 0 8) (bvmul a b))
+  (bvmul a b)
   )
 
-(define (same-mult original example a b)
-  ;; (define b (bv 2 8))
+;; (println (simple-mult (bv 2 8) (bv 5 8) (vector 0 0)))
+
+(define (same-mult original example a)
+  (define b (bv 5 8))
   (define regs (make-vector 12 (bv 0 8)))
   (vector-set! regs 11 (bv 1 8))
   (vector-set! regs 10 (bv -1 8))
@@ -175,8 +173,8 @@
 (define-symbolic b (bitvector 8))
 
 (define sol
-  (synthesize #:forall (list a b)
-              #:guarantee (same-mult simple-mult loop-template a b)))
+  (synthesize #:forall a
+              #:guarantee (same-mult simple-mult loop-template a)))
 
 (print-forms sol)
 
